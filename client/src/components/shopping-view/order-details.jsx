79a91d20423ed716c05f23d4492a28ccc5949db2
SasithaRh <ruvishansth1030@gmail.com>
2025-04-27 21:20:58 +0530

    Completed user side orders section

--- client/src/components/shopping-view/order-details.jsx
+++ client/src/components/shopping-view/order-details.jsx
@@ -4,7 +4,7 @@ import { DialogContent } from "../ui/dialog";
 import { Label } from "../ui/label";
 import { Separator } from "../ui/separator";
 
-function ShoppingOrderDetailsView() {
+function ShoppingOrderDetailsView({ orderDetails }) {
   const { user } = useSelector((state) => state.auth);
 
   return (
@@ -13,31 +13,37 @@ function ShoppingOrderDetailsView() {
         <div className="grid gap-2">
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order ID</p>
-            <Label>1232</Label>
+            <Label>{orderDetails?._id}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Date</p>
-            <Label>21321321</Label>
+            <Label>{orderDetails?.orderDate.split("T")[0]}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Price</p>
-            <Label>$213213213</Label>
+            <Label>${orderDetails?.totalAmount}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Payment method</p>
-            <Label>paypal</Label>
+            <Label>{orderDetails?.paymentMethod}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Payment Status</p>
-            <Label>pending</Label>
+            <Label>{orderDetails?.paymentStatus}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Status</p>
             <Label>
               <Badge
-                className={`py-1 px-3 bg-green-500`}
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === "confirmed"
+                    ? "bg-green-500"
+                    : orderDetails?.orderStatus === "rejected"
+                    ? "bg-red-600"
+                    : "bg-black"
+                }`}
               >
-                confirmed
+                {orderDetails?.orderStatus}
               </Badge>
             </Label>
           </div>
@@ -47,7 +53,15 @@ function ShoppingOrderDetailsView() {
           <div className="grid gap-2">
             <div className="font-medium">Order Details</div>
             <ul className="grid gap-3">
-             
+              {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
+                ? orderDetails?.cartItems.map((item) => (
+                    <li className="flex items-center justify-between">
+                      <span>Title: {item.title}</span>
+                      <span>Quantity: {item.quantity}</span>
+                      <span>Price: ${item.price}</span>
+                    </li>
+                  ))
+                : null}
             </ul>
           </div>
         </div>
@@ -56,7 +70,11 @@ function ShoppingOrderDetailsView() {
             <div className="font-medium">Shipping Info</div>
             <div className="grid gap-0.5 text-muted-foreground">
               <span>{user.userName}</span>
-              
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
             </div>
           </div>
         </div>
--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -10,9 +10,13 @@ import {
   TableHeader,
   TableRow,
 } from "../ui/table";
-
-import { useDispatch, useSelector } from "react-redux";
 import ShoppingOrderDetailsView from "./order-details";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllOrdersByUserId,
+  getOrderDetails,
+  resetOrderDetails,
+} from "@/store/shop/order-slice";
 import { Badge } from "../ui/badge";
 
 function ShoppingOrders() {
@@ -21,9 +25,13 @@ function ShoppingOrders() {
   const { user } = useSelector((state) => state.auth);
   const { orderList, orderDetails } = useSelector((state) => state.shopOrder);
 
+  function handleFetchOrderDetails(getId) {
+    dispatch(getOrderDetails(getId));
+  }
 
-
- 
+  useEffect(() => {
+    dispatch(getAllOrdersByUserId(user?.id));
+  }, [dispatch]);
 
   useEffect(() => {
     if (orderDetails !== null) setOpenDetailsDialog(true);
@@ -40,7 +48,7 @@ function ShoppingOrders() {
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>Order ID</TableHead>
+              <TableHead className="ml-9">Order ID</TableHead>
               <TableHead>Order Date</TableHead>
               <TableHead>Order Status</TableHead>
               <TableHead>Order Price</TableHead>
@@ -50,39 +58,46 @@ function ShoppingOrders() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            
+            {orderList && orderList.length > 0
+              ? orderList.map((orderItem) => (
                   <TableRow>
-                    <TableCell>2321</TableCell>
-                    <TableCell>122321</TableCell>
+                    <TableCell>{orderItem?._id}</TableCell>
+                    <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
                     <TableCell>
                       <Badge
-                        className={`py-1 px-3 bg-green-500`
-                           }
+                        className={`py-1 px-3 ${
+                          orderItem?.orderStatus === "confirmed"
+                            ? "bg-green-500"
+                            : orderItem?.orderStatus === "rejected"
+                            ? "bg-red-600"
+                            : "bg-black"
+                        }`}
                       >
-                        confirmed
+                        {orderItem?.orderStatus}
                       </Badge>
                     </TableCell>
-                    <TableCell>$213213</TableCell>
+                    <TableCell>${orderItem?.totalAmount}</TableCell>
                     <TableCell>
                       <Dialog
                         open={openDetailsDialog}
-                        onOpenChange={
-                          setOpenDetailsDialog
-                        
-                        }
+                        onOpenChange={() => {
+                          setOpenDetailsDialog(false);
+                          dispatch(resetOrderDetails());
+                        }}
                       >
                         <Button
                           onClick={() =>
-                            setOpenDetailsDialog(true)
+                            handleFetchOrderDetails(orderItem?._id)
                           }
                         >
                           View Details
                         </Button>
-                        <ShoppingOrderDetailsView  />
+                        <ShoppingOrderDetailsView orderDetails={orderDetails} />
                       </Dialog>
                     </TableCell>
                   </TableRow>
-               
+                ))
+              : null}
           </TableBody>
         </Table>
       </CardContent>
